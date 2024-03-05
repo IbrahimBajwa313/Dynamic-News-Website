3f437977ece4a7bc0cd3c89d978650f814ab1630
IbrahimBajwa313 <ibrahimbajwa1065@gmail.com>
2024-03-05 09:35:55 +0500

    Security added to dont allow unauthorized user to access the images

---
+++ admin/upload/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Not Found</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container"> 
+        <h1>Oops! Page Not Found</h1>
+        <p>We couldn't find the page you're looking for.</p>
+        <!-- <p>But don't worry, you can <a href="{$hostname}/admin/post.php">go back to the homepage</a> or try searching.</p>  -->
+    </div>
+</body>
+</html>
